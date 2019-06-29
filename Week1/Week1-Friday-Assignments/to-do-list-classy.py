4a5f66622ed213588c792a72ac3c534307cc5d4c
Jacob Bankston <jacob.bankston.smile@gmail.com>
2019-06-29 16:38:50 -0500

    to-do-list-classy.py works now. Made the to-do-list app with class instead of dictionaries.

--- Week1/Week1-Friday-Assignments/to-do-list-classy.py
+++ Week1/Week1-Friday-Assignments/to-do-list-classy.py
@@ -1,58 +1,121 @@
-task_list = []
-
-class Task():
+class Task:
     def __init__(self, title, priority):
         self.title = title
         self.priority = priority
 
+task_list = []
+
+# class Task_List():
+#     def __init__(self, name):
+#         self.name = name
+
+#     def add_task(self, title, priority):
+#         self.title = input("Please select a Title for the Task: ")
+#         while True:
+#             priority_input = input("Please select a Priority Level for the Task [High, Medium, Low]")
+#             if priority_input == ("l" or "L" or "low" or "Low" or "LOW"):
+#                 self.priority = "LOW"
+#             elif priority_input == ("m" or "M" or "medium" or "Medium" or "MEDIUM"):
+#                 self.priority = "MEDIUM"
+#             elif priority_input == ("h" or "H" or "high" or "HIGH" or "High"):
+#                 self.priority = "HIGH"
+#             else:
+#                 print("Input not entered correctly.")
+    
+#     def del_task(self, task):1
+#         del task
+#         print("Task Deleted Successfully!")
 
 def add_task_input():
-    title_input = input("Please select a Title for the Task: ")
+    title = input("Please select a Title for the Task: ")
     while True:
-        priority_input = input("Please select a Priority Level for the Task [High, Medium, Low]")
-        if priority_input == ("l" or "L" or "low" or "Low" or "LOW"):
-            priority = "LOW"
-        elif priority_input == ("m" or "M" or "medium" or "Medium" or "MEDIUM"):
-            priority = "MEDIUM"
-        elif priority_input == ("h" or "H" or "high" or "HIGH" or "High"):
-            priority = "HIGH"
+        priority_input = input("Please select a Priority Level for the Task [High - h, Medium - m, Low - l]: ")
+        priority = priority_levels(priority_input)
+        if priority == False:
+            print("ERROR: Input not entered correctly.\n")
         else:
-            print("Input not entered correctly.")
-    return (title_input, priority)
+            break
+    return (title, priority)
 
-def remove_task_input():
-    view_task_list()
-    user_input = input("Select which task you would like to delete: ")
-    task_list.remove(str(user_input))
-    print("Task Deleted Successfully!")
+def priority_levels(pinput):
+    if 'l' in pinput:
+        return "LOW"
+    if 'm' in pinput:
+        return "MEDIUM"
+    if 'h' in pinput:
+        return "HIGH"
+    else:
+        return False
+
+def task_creator(title, priority):
+    new_task = Task(title, priority)
+    return new_task
+
+def remove_task_input(task_to_remove):
+    task_list.remove(task_list[task_to_remove - 1])
+    print("Task Deleted Successfully!\n")
 
 def view_task_list():
     if len(task_list) == 0:
-        print("There are no tasks in your task list.")
+        print("There are no tasks in your task list.\n")
     else:
         counter = 1
         for item in task_list:
-            print(counter + item.title + item.priority)
+            print(f"{spacing2}{counter} - {item.title} - {item.priority}")
             counter += 1
-    
-
-
 
 user_input = ""
+out_going_message = "Thanks for using the Task List Manager app!\n+++          I Love You!          +++"
+spacing = "        + "
+spacing2 = "          "
+
+print("       ++++++++++++++++++++++\nWelcome to your new Task List Manager!\n       ++++++++++++++++++++++")
 
 while user_input != "q":
-    user_input = input("Press 1 to add task\nPress 2 to delete task\nPress 3 to view all tasks\nPress q to quit ")
+    print("           ++ Main Menu ++")
+    counter = 0
+    user_input = input(f"{spacing}Press 1 to add task\n{spacing}Press 2 to delete task\n{spacing}Press 3 to view all tasks\n{spacing}Press q to quit\n")
+
     if user_input == "1":
-        add_task_input()
+        add_input = "a"
+        while add_input == "a":
+            title, priority = add_task_input()
+            task_list.append(task_creator(title, priority))
+            add_input = input(f"{spacing}If you would like to add another task press 'a'.\n{spacing}Press any key to return to the main menu.\n")
+        counter = 1
 
     if user_input == "2":
-        pass
-    if user_input == "3":
+        print("-----------------------------------------------------------------------------------------------------")
         view_task_list()
-        user_response = input("Press m to return to menu, or press q to quit.")
+        while counter == 0:
+            delete_input = input(f"{spacing}Select which task you would like to delete, or press c to cancel: ")
+            if delete_input == "c":
+                counter = 1
+                pass
+            else:
+                try:
+                    number = int(delete_input)
+                    if number > len(task_list) or number < 1:
+                        print("ERROR: Entered value is not in the task list.\n")
+                    else:
+                        remove_task_input(number)
+                        counter = 1
+                except ValueError:
+                    print("ERROR: Please enter a correct reponse.\n")
+
+
+    if user_input == "3":
+        print("        ++                             +++                            ++    ")
+        view_task_list()
+        user_response = input(f"{spacing}Press m to return to menu, or press q to quit.\n")
+        counter = 1
         if user_response == "q":
-            user_input = "q"
+            print(out_going_message)
+            break
+
     if user_input == "q":
+        print(out_going_message)
         break
-    else:
-        print("Please select one of the available options!")
+
+    elif counter == 0:
+        print("ERROR: Please select one of the available options!")
